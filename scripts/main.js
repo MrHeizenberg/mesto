5d1f5db202b47b7a655700c5a7652282d3b663ee
Nikolay Avdeev <avdeevklj@mail.ru>
2021-05-22 01:08:05 +0300

    Sprint4v2

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,28 @@
-let popupOpen = document.querySelector(".profile__edit");
-let popup = document.querySelector(".popup");
-let popupClose = document.querySelector(".popup__close");
+let popupOpen = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let author = document.querySelector('.profile__title');
+let descriptionAuthor = document.querySelector('.profile__subtitle');
+let jobInput = document.querySelector('input[name=author]');
+let nameInput = document.querySelector('input[name=description]');
+let form = document.querySelector('.popup__infosave');
 
-popupOpen.addEventListener("click", toggleClass)
-popupClose.addEventListener("click", toggleClass)
-
-function toggleClass() {
-    popup.classList.toggle("popup_open");
+function openPopup() {
+    nameInput.value = author.textContent;
+    jobInput.value = descriptionAuthor.textContent;
+    popup.classList.add('popup_open');
 }
 
-let Author = document.querySelector(".profile__title");
-let descriptionAuthor = document.querySelector(".profile__subtitle");
-let nameInput = document.querySelector(".popup__text_author");
-let jobInput = document.querySelector(".popup__text_description");
-nameInput.value = Author.textContent;
-jobInput.value = descriptionAuthor.textContent;
+function closePopup() {
+    popup.classList.remove('popup_open');
+}
 
-popupClose.addEventListener("click", function() {
-    nameInput.value = Author.textContent;
-    jobInput.value = descriptionAuthor.textContent;
-})
-
-let submit = document.querySelector(".popup__save");
-submit.addEventListener("click", function() {
-    Author.textContent = nameInput.value;
+function save() {
+    author.textContent = nameInput.value;
     descriptionAuthor.textContent = jobInput.value;
-    popup.classList.toggle("popup_open");
-}); 
+    closePopup();
+}
 
-let like = document.querySelectorAll(".cards__like");
-
-for (let i = 0; i < like.length; i++) {
-like[i].addEventListener("click", toggleClassLike) 
-function toggleClassLike() {
-    like[i].classList.toggle("cards__like_active")}};
+popupOpen.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
+form.addEventListener('submit', save);
